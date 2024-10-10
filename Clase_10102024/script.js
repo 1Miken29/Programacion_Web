6752128566d6fffdf82a5556cf1b0295dc12ad99
1Miken29 <152199511+1Miken29@users.noreply.github.com>
2024-10-10 16:44:43 -0600

    Funciones

---
+++ Clase_10102024/script.js
@@ -0,0 +1,15 @@
+//comentarios de una linea
+/* 
+comentarios multi
+li
+ne
+a
+*/
+//declaramos nuestra funcion
+function hello(){
+    return 'hola js';
+}
+console.log(hello());
+//Investigar diferentes usos de funciones
+//en JS y ejemplificar cada una de ellas
+//con uso de parametros, funcion dentro de otra funcion
