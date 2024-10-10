9d12469ccf813b6b85c0a9ef0f7908b925fa5312
1Miken29 <152199511+1Miken29@users.noreply.github.com>
2024-10-10 16:38:34 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+//Clase 24/09/2024
+//Nombre: Cruces Pelaez Miguel
+//Variables
+
+//las variables guardan informacion que puedes reutilizar
+//declaracion de variables
+
+let nombre = 'Pedro'; //usa let para declarar variables que pueden cambiar su valor
+const PI = 3.1416;
+var edad = 19;
+
+let a = 5;
+let b = 2;
+
+let suma = a + b;
+let resta = a - b;
+let multiplicacion = a * b;
+let division = a / b;
+
+//operadores de comparacion
+
+a > b; //true
+a < b; //false
+a == b; //false
+a === b; //false(compara alores y tipo)
